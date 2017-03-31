82a9090b5b9a44ef8e2ef3bc76e2ecf88ed6db48
kong0107 <kong0107@gmail.com>
2017-03-31 08:35:31 +0800

    Handle some `read more`

--- README.md
+++ README.md
@@ -10,13 +10,16 @@ backup timeline posts of one other person by running scripts in browser console
 * 本專案用的不是正規的方法，資料可能不齊全、方法亦隨時可能失效。
 
 ## 進度
+* 不包含相簿與照片；可另參考 [Ensky's Album Downloader for Facebook](https://chrome.google.com/webstore/detail/enskys-album-downloader-f/oallcdoceahndjmaalbicbcgpfnajgae) ，不過那似乎只有儲存照片，而沒有針對照片的留言討論。
+* 不包含「網誌」的全文。
 * 僅確認 Google Chrome 瀏覽器可以運作。
-* 可以抓到最初的文章，但有很多「更多」（例如很多則留言時，部分會被隱藏；以及單則留言很長時亦會被截斷）還沒有處理。
 * 還有很多不必要的東西（例如無法操作的「按讚」）會被顯示出來。
+* 未做細部排版；所有東西都擠在同一頁，開啟時間很久。
 
 ## 使用
 1. 開啟欲備份的臉書個人動態時報。
 2. 開啟瀏覽器的開發者工具（快捷鍵 `F12` ），進入「主控台 (Console) 」。
 3. 將 `main.js` 的內容貼進主控台，並按下 `Enter` 開始執行。
 4. 讓畫面自己捲動，直到沒有東西可以再捲之後十幾秒，主控台出現 `Auto-scroll stopped.` 訊息。
-5. 若出現 `Auto-scroll stopped.` 訊息後數十秒仍沒有自動觸發下載，那就是失敗了。
+5. 出現 `Auto-scroll stopped.` 訊息後數秒內應會自動觸發下載，若沒有出現那就是失敗了。
+6. 開啟下載好的檔案（因檔案較大，開啟時間可能超過十秒），然後按下 `Ctrl + S` 另存新檔，「存檔類型」選擇「完整的網頁」而非「僅限網頁的 HTML 部分」，瀏覽器即會再自動將圖檔也統一複製進一個資料夾。
--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+/**
+ * 備份（別人的）塗鴉牆文章。
+ *
+ * 程式自動拉到底之後會先下載一次，名為 `unexpanded.html` ；
+ * 點完所有的「更多」之後會下載，名為 `whole.html` ；
+ * 簡化掉不重要的元件和屬性後，會再存一份，名為 `simplified` 。
+ */
+
+var title = "untitled";
+
 /// 這一段可以一直拉到最舊的文章
 var delay = 2000;
 var timer = setInterval(function() {
@@ -9,8 +19,11 @@ var timer = setInterval(function() {
             if(failCounter++ == 5) {
                 clearInterval(timer);
                 console.log("Auto-scroll stopped.")
-                if(typeof export_recent_capsule_container == "function")
-                    export_recent_capsule_container();
+                console.log("Downloading without expansion ...");
+                triggerDownload("unexpanded.html", wrappingHTML(
+                    document.getElementById("recent_capsule_container").innerHTML
+                ));
+                expandAll();
             }
         }
         else {
@@ -21,41 +34,137 @@ var timer = setInterval(function() {
 }(), delay);
 
 
+/**
+ * 幾個須處理的「更多」：
+ * 「查看其他 {count} 則貼文」：出現於有較多其他人來牆上貼文時。選擇器 `.showAll > ._4qba` ，但要似乎最後一個是會造成卡住的「顯示全部」（待確認）。
+ * 「檢視另{count}則留言」：出現於單篇文章的留言筆數較多時。選擇器 `.UFIPagerLink` 。
+ * 「查看另 {count} 則留言」：與前同，不知為何會有兩種格式。選擇器亦與前同。
+ * 「{author}{verified_badge}已回覆」「{count}則回覆」：針對回應的回應。選擇器 `.UFICommentLink` 。
+ * 「查看更多」：單一留言字數較多時。選擇器 `.fss` 和 `._5v47` 結果相同。（內容似乎不是用 AJAX 取得，但亦非存在 DOM tree 裡，所以還是要點一下）
+ * 「更多」：出現於貼文主內容字數較多時，包含別人貼的，以及轉貼別人的臉書文章。好像會觸發跳頁，而且元素已經存在於 DOM tree ，所以不處理也可以。
+ * 「繼續閱讀」：出現於使用者發表的網誌，點了不會顯示出剩餘內容，而是會跳頁－－也就是本程式無法備份到的部分。
+ */
+var timer;
+var keepClick = function() {
+    var target, counter, total;
+    var init = function() {
+        target = false;
+        counter = 0;
+        total = -1;
+    };
+    init();
+    return function(con, selector, callback) {
+        if(total < 0) {
+            total = con.querySelectorAll(selector).length;
+            if(!total) {
+                console.log("There's no " + selector + " in the " + con);
+                init();
+                if(callback) timer = setTimeout(callback, delay);
+                return;
+            }
+            console.log(selector + " is found " + total + " time(s) in the " + con);
+        }
+        if(!target || !con.contains(target)) {
+            target = con.querySelector(selector);
+            if(!target) {
+                console.log("Clicked all " + selector + " in the " + con);
+                init();
+                if(callback) timer = setTimeout(callback, delay);
+                return;
+            }
+            console.log(++counter + "/" + total);
+            target.click();
+        }
+        timer = setTimeout(function() {
+            keepClick(con, selector, callback);
+        }, delay);
+    };
+}();
 
-///// 幾個待處理的「更多」
-/// 檢視另 .UFIPagerLink
-/// 查看其他 ._44b2
-/// 查看更多
+
+/// 這樣寫很醜但有用。
+function expandAll() {
+    var con = document.getElementById("recent_capsule_container");
+    keepClick(con, ".showAll > ._44b2", function() {
+        keepClick(con, ".UFIPagerLink", function() {
+            keepClick(con, ".UFICommentLink", function() {
+                keepClick(con, ".fss._5v47", simplifyAndDownload);
+            });
+        });
+    });
+}
 
 
+/// 把需要的資料輸出成檔案，並湊齊完整的 HTML 檔案。
+function simplifyAndDownload() {
+    var con = document.getElementById("recent_capsule_container");
 
-/// 把需要的資料輸出成檔案，並湊齊完整的 HTML 檔案、加上些微排版。
+    console.log("Downloading the whole container ...");
+    triggerDownload("whole.html", wrappingHTML(con.innerHTML));
 
-//var html = '<!DOCTYPE HTML><html lang="zh-TW"><head><meta charset="UTF-8"><base href="https://www.facebook.com/" target="_blank"><title>Thomas Chen</title><link rel="stylesheet" href="fb.css"></head><body>'
-/*var html = '<!DOCTYPE HTML><html lang="zh-TW"><head><meta charset="UTF-8"><title>Thomas Chen</title><link rel="stylesheet" href="fb.css"></head><body>'
-    + document.getElementById("recent_capsule_container").innerHTML
-    + '</body></html>';*/
+    console.log("Copying ...");
+    var copy = con.cloneNode(true);
 
-function export_recent_capsule_container() {
-    var onload = '(function(nl){for(var i=0;i<nl.length;++i)nl[i].style.cssText="";})(document.querySelectorAll("[style]"))';
-    var css = '.see_more_link_inner, .fbTimelineSubSections, .loadingIndicator  { display: none; } .userContentWrapper  { border-top: 4px solid #888; padding: .5rem; } .UFIContainer { margin-left: .5rem; border-left: 2px solid #888; padding-left: .5rem; } ._5x46 * { display: inline; }';
+    /**
+     * 刪掉不需要的節點
+     */
+    console.log("Deleting unnecessary nodes ...");
+    var elems = copy.querySelectorAll("*");
+    for(var i = 0; i < elems.length; ++i) {
+        var ele = elems[i];
+        if(!copy.contains(ele)) continue;
+        if(ele.tagName != "IMG" && !ele.querySelector("img") && !ele.textContent)
+            ele.parentNode.removeChild(ele);
+    }
 
+    /**
+     * 刪掉不需要的屬性
+     */
+    console.log("Deleting unnecessary attributes ...");
+    var elems = copy.querySelectorAll("*");
+    for(var i = 0; i < elems.length; ++i) {
+        var ds = elems[i].dataset;
+        for(var j in ds) delete ds[j];
+
+        var attrs = elems[i].attributes;
+        for(var j = 0; j < attrs.length; ++j) {
+            var attr = attrs[j].name;
+            if( attr == "onclick"
+                || attr == "ajaxify"
+                || (attr == "style" && !elems[i].style.backgroundImage) ///< 貼圖是用 `<div style="background-image: url(xxx);"></div>` 做的。
+                || (attr == "href" && elems[i].getAttribute("href") == "#") ///< 注意跟 `elems[i].href` 不同。
+                //|| attr.match(/^data-/) ///< 用 `Element.dataset` 物件處理即可
+                //|| attr.match(/^aria-/) ///< 身為手天使成員，還是不要刪這個吧。而且 `aria-label` 在按讚／怒數那邊可以用。
+            )
+            elems[i].removeAttribute(attr);
+        }
+    }
+
+    console.log("Downloading the simplified version ...");
+    triggerDownload("simplified.html", wrappingHTML(copy.innerHTML));
+    console.log("Done!");
+}
+
+function wrappingHTML(body) {
+    var css = '';//'.see_more_link_inner, .fbTimelineSubSections, .loadingIndicator  { display: none; } .userContentWrapper  { border-top: 4px solid #888; padding: .5rem; } .UFIContainer { margin-left: .5rem; border-left: 2px solid #888; padding-left: .5rem; } ._5x46 * { display: inline; }';
     var html = '<!DOCTYPE HTML>\
     <html lang="zh-TW">\
         <head>\
             <meta charset="UTF-8">\
-            <title>Thomas Chen</title>\
+            <title>' + title + '</title>\
             <base href="https://www.facebook.com/" target="_blank">\
-            <script>document.body.addEventListener("load", function(){' + onload + '});</script>\
             <style>' + css + '</style>\
         </head>\
         <body>';
-    html += document.getElementById("recent_capsule_container").innerHTML;
-    html += '</body></html>';
+    html += body + '</body></html>';
+    return html;
+}
 
+function triggerDownload(filename, content, type) {
+    if(!type) type = "text/html";
     var a = document.createElement("a");
-    var blob = new Blob([html], {type: "text/html"});
+    var blob = new Blob([content], {type: type});
     a.href = URL.createObjectURL(blob);
-    a.download = "static.html";
+    a.download = filename;
     a.click();
 }
